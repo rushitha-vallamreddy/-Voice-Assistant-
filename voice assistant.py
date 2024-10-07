3d3b67249edfb690d1c77b4a516d809b9e71ddbe
RUSHITHA_VALLAMREDDY <sahirushi9@gmail.com>
2024-10-07 23:27:59 +0530

    Add files via upload

---
+++ voice assistant.py
@@ -0,0 +1,13 @@
+import pyttsx3
+
+def say(text):
+    engine=pyttsx3.init()
+    voice=engine.getProperty('voices')
+    engine.setProperty('voice',voice[1].id)
+    engine.say(text)
+    engine.runAndWait()
+
+while True:
+    text=input(print("Enter Any Text!"))
+    say(text)
+
